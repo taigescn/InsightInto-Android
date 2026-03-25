9c17e48a58b57cb89aab0112fb7e1ba16c24deaf
Taiges <taigescn@gmail.com>
2026-03-25 10:39:30 +0800

    [1.0.0]<调整服务端返回的采集项上报间隔配置字段名>

--- insight-base/src/main/java/com/taiges/insight/into/bean/api/ReaderConfig.kt
+++ insight-base/src/main/java/com/taiges/insight/into/bean/api/ReaderConfig.kt
@@ -6,7 +6,7 @@ package com.taiges.insight.into.bean.api
  * @param isFollowEvent 是否跟随事件上报该采集项，0：否，1：是
  * @param isTimeConsuming 是否为耗时采集项，0：否，1：是
  * @param cacheTime 采集项缓存时间(毫秒)
- * @param intervalTime 采集项采集间隔时间(毫秒)
+ * @param uploadIntervalTime 采集项采集上报间隔时间(毫秒)
  */
 class ReaderConfig(
     val itemId: Int,
@@ -14,7 +14,7 @@ class ReaderConfig(
     val isFollowEvent: Int,
     val isTimeConsuming: Int,
     val cacheTime: Long?,
-    val intervalTime: Long?,
+    val uploadIntervalTime: Long?,
 ) {
     /**
      * 是否需要本地缓存数据
